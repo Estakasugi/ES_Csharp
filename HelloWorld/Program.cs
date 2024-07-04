218141b5f9b9281ecac75eec298a61dfbd2e8379
ES_Takasugi <168489667+Estakasugi@users.noreply.github.com>
2024-07-04 00:15:54 -0400

    Add files via upload

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,5 @@
+﻿string firstName = "Bob";
+int numOfMessage = 3;
+double temperature = 34.4;
+Console.Write("Hello, " + firstName + "! You have " + numOfMessage 
+                + " messages in your inbox. The temperature is " + temperature + " celsius.");
